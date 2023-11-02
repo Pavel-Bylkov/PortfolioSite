3601d347fdc9aa99dc3c9663c9c5295093a13dd6
davidlubomirsky <david@lubomirsky.com>
2023-11-02 15:16:02 -0400

    added mainapp

--- PortfolioSite/mainapp/views.py
+++ PortfolioSite/mainapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context = {}
+    contacts = {"email":"david@lubomirsky.com", "phone":"(732) 915-2434", "website":" http://127.0.0.1:8000/", "linkedin":"", "github":"", "twitter":"", "snapchat":"bcecam1"}
+    context = {"vars": "hello world", "name":"David Lubomirsky", "description":"profesional gamer", "contacts":contacts}
     return render(request, 'mainapp/index.html', context)
