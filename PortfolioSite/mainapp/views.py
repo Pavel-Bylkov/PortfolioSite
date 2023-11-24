957e5cc36aabf220493179ab1bee4fe6e8977308
Pavel-Bylkov <oldman.bpv@gmail.com>
2023-11-24 07:21:39 +0700

    fix migrations

--- PortfolioSite/mainapp/views.py
+++ PortfolioSite/mainapp/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 
-from mainapp.models import Person
+from mainapp.models import *
 
 
 # Create your views here.
 def home(request):
 
     user = Person.objects.all()
+    if not user:
+        prof = Profession.objects.create(title="Developer")
+        prof.save()
+        user = Person.objects.create(first_name="John",
+                                     last_name="Conor",
+                                     email="john_conor@mail.ru",
+                                     number="(732) 915-2434",
+                                     url="site.john_conor.ru",
+                                     profession=prof,
+                                     roles={})
+        user.save()
+        user = Person.objects.all()
     print(user[0].first_name)
-    contacts = {"email":"david@lubomirsky.com", "phone":"(732) 915-2434", "website":" http://127.0.0.1:8000/", "linkedin":"", "github":"", "twitter":"", "snapchat":"bcecam1"}
-    context = {"vars": "hello world", "name":f"{user[0].first_name} {user[0].last_name}", "description":"profesional gamer", "contacts":contacts}
+    contacts = {"email": user[0].email, "phone":user[0].number, "website": f"http://{user[0].url}/", "linkedin":"", "github":"", "twitter":"", "snapchat":"bcecam1"}
+    context = {"vars": "hello world", "name": f"{user[0].first_name} {user[0].last_name}", "description": user[0].profession.title, "contacts":contacts}
     return render(request, 'mainapp/index.html', context)
